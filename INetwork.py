71b429ab6634bae1e130d5b6bec4c61762293161
glados17 <287411139@qq.com>
2020-11-04 20:12:40 -0600

    'fir'

--- INetwork.py
+++ INetwork.py
@@ -11,21 +11,22 @@ import time
 import argparse
 import warnings
 import tensorflow as tf
-from keras.models import Model
-from keras.layers import Input
-from keras.layers.convolutional import Convolution2D, AveragePooling2D, MaxPooling2D
-from keras import backend as K
-from keras.utils.data_utils import get_file
-from keras.utils.layer_utils import convert_all_kernels_in_model
+
+# from keras.models import Model
+# from keras.layers import Input
+# from keras.layers.convolutional import Convolution2D, AveragePooling2D, MaxPooling2D
+# from keras import backend as K
+# from keras.utils.data_utils import get_file
+# from keras.utils.layer_utils import convert_all_kernels_in_model
 
 tf.compat.v1.disable_eager_execution()
 
-# from tensorflow.keras.models import Model
-# from tensorflow.keras.layers import Input
-# from tensorflow.keras.layers import Convolution2D, AveragePooling2D, MaxPooling2D
-# from tensorflow.keras import backend as K
-# from tensorflow.keras.utils import get_file
-# from tensorflow.keras.utils import convert_all_kernels_in_model
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Convolution2D, AveragePooling2D, MaxPooling2D
+from tensorflow.keras import backend as K
+from tensorflow.keras.utils import get_file
+from tensorflow.keras.utils import convert_all_kernels_in_model
 
 """
 Neural Style Transfer with Keras 2.0.5
